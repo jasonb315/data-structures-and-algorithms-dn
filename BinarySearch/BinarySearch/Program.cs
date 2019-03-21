e8e9155b7791d40bb822cbb3f9a1b55a52ebf27a
Jason Burns <jasonb315@protonmail.com>
2019-03-20 23:16:30 -0700

    console inputs

--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -6,17 +6,43 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string appName = "ATM";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jason Burns";
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Enter array length");
+            string inputStr = Console.ReadLine();
+            int arrLen = Convert.ToInt32(inputStr);
+            int[] arr = new int[arrLen];
+
+            foreach(int i in arr)
+            {
+                Console.WriteLine("Enter sorted array values");
+                string iStr = Console.ReadLine();
+                int iVal = Convert.ToInt32(inputStr);
+                arr[i] = iVal;
+            }
+
+            Console.WriteLine("Enter search key");
+            string keyStr = Console.ReadLine();
+            int keyVal = Convert.ToInt32(inputStr);
+            int key = arrLen;
+
+            int iReturn = BinarySearch(arr, key);
+            Console.WriteLine(iReturn);
+
         }
         static int BinarySearch(int[]arr, int key)
         {
             int min = 0;
             int max = arr.Length - 1;
+            int mid = 0;
 
             while (min <= max)
             {
-                double x = (min + max) / 2;
-                int mid = (int)Math.Floor(x);
+                mid = (min + max) / 2;
 
                 if (arr[mid] < key)
                 {
