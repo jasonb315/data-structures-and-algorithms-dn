a7d26f7d40ded2ac85a2405053f98cd6a3ea77ab
Jason Burns <jasonb315@protonmail.com>
2019-03-24 21:38:14 -0700

    Linked list method count

--- DataStructures/LinkedList/LinkedList/Classes/LinkedList.cs
+++ DataStructures/LinkedList/LinkedList/Classes/LinkedList.cs
@@ -6,5 +6,63 @@ namespace LinkedList.Classes
 {
     class LinkedList
     {
+        public Node Head { get; set; }
+
+        public void Insert()
+        {
+            //new node to head
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Append()
+        {
+            // new node to tail
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Includes()
+        {
+            // bool for key
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Print()
+        {
+            //read off
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
