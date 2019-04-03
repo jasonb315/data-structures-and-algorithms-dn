fd9eb45c7174373a4583ecdc35354bf327a8df8c
Jason Burns <jasonb315@protonmail.com>
2019-04-03 09:05:35 -0700

    ini

---
+++ Challenges/FifoAnimalShelter/FifoAnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues.Classes;
+
+namespace FifoAnimalShelter.Classes
+{
+    public class AnimalShelter
+    {
+        public Queue dogs = new Queue();
+        public Queue cats = new Queue();
+        public int tagID = 0;
+        // no vars passed on instantiation
+
+        public void Enqueue(Animal ani)
+        {
+            ani.ID = tagID;
+            tagID++;
+            dogs.Enqueue(ani);
+        }
+
+        public Animal Dequeue(string species)
+        {
+            if ( species.ToLower() == "dog" )
+            {
+
+            }
+            else if ( species.ToLower() == "cat")
+            {
+
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+
+}
