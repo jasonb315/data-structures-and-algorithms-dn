fd3f1273ab301263602fce1955b78c3e89d9b12f
Jason Burns <jasonb315@protonmail.com>
2019-04-03 17:26:37 -0700

    bracket pair dictionary, string split, split foreach

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MultiBracketValidation("[{The {quick([{ }])brown} fox}]");
+        }
+
+    
+
+        public static bool MultiBracketValidation(string input)
+        {
+            Queue bracketQueue = new Queue();
+
+            var bracketPairs = new Dictionary<string, string>() { {"{", "}"}, {"[", "]"}, {"(", ")"} };
+
+            foreach (char item in input)
+            {
+                if (bracketPairs.ContainsKey(item.ToString()))
+                {
+                    Console.WriteLine("Ding");
+                }
+            }
+            return true;
+        }
+    }
+}
