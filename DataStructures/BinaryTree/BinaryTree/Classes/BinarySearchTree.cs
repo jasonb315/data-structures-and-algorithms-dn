27cecfb10ca869c05059d78871ef637f51e2a649
Jason Burns <jasonb315@protonmail.com>
2019-04-07 12:59:11 -0700

    BST initial solution, summary strings

--- DataStructures/BinaryTree/BinaryTree/Classes/BinarySearchTree.cs
+++ DataStructures/BinaryTree/BinaryTree/Classes/BinarySearchTree.cs
@@ -8,6 +8,11 @@ namespace BinaryTree.Classes
     {
         public static BTNode<T> Root { get; set; }
 
+        /// <summary>
+        ///     Conditionally places new node in tree based on given ID val
+        /// </summary>
+        /// <param name="ID">int node.ID</param>
+        /// <param name="data">T node.Data</param>
         public void Add(int ID, T data)
         {
             BTNode<T> newNode = new BTNode<T>(data);
@@ -19,40 +24,78 @@ namespace BinaryTree.Classes
                 Root = newNode;
                 return;
             }
-
-            // root? start at root
-            BTNode<T> currentNode = Root;
-
-            while (true)
+            else
             {
+                // root? start at root
+                BTNode<T> currentNode = Root;
 
-                if (newNode.ID > currentNode.ID && currentNode.Right == null)
+                while (true)
                 {
-                    currentNode.Right = newNode;
-                    return;
-                }
 
-                else if (newNode.ID < currentNode.ID && currentNode.Left == null)
-                {
-                    currentNode.Left = newNode;
-                    return;
-                }
+                    if (newNode.ID > currentNode.ID && currentNode.Right == null)
+                    {
+                        currentNode.Right = newNode;
+                        return;
+                    }
 
-                else if (newNode.ID > currentNode.ID && currentNode.Right != null)
-                {
-                    currentNode = currentNode.Right;
-                }
+                    else if (newNode.ID < currentNode.ID && currentNode.Left == null)
+                    {
+                        currentNode.Left = newNode;
+                        return;
+                    }
 
-                else if (newNode.ID < currentNode.ID && currentNode.Left != null)
-                {
-                    currentNode = currentNode.Left;
-                }
+                    else if (newNode.ID > currentNode.ID && currentNode.Right != null)
+                    {
+                        currentNode = currentNode.Right;
+                    }
 
+                    else if (newNode.ID < currentNode.ID && currentNode.Left != null)
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                }
             }
         }
 
-        // contains
+        /// <summary>
+        ///     conditionally walks down tree checking for value match
+        /// </summary>
+        /// <param name="key">Node ID</param>
+        /// <returns>bool for key node.Id match</returns>
+        public bool Contains(int key)
+        {
+            if (Root == null)
+            {
+                return false;
+            }
+            else
+            {
+                BTNode<T> currentNode = Root;
+
+                while (true)
+                {
+                    if (currentNode.ID == key)
+                    {
+                        return true;
+                    }
+                    if (currentNode.ID > key && currentNode.Left != null)
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                    else if (currentNode.ID < key && currentNode.Right != null)
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                    else if(currentNode.ID < key && currentNode.Right == null)
+                    {
+                        return false;
+                    }
+                    else if(currentNode.ID > key && currentNode.Left == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
-
-
 }
