34f2502142ad565734bd5e0cda9065396e5a1242
Jason Burns <jasonb315@protonmail.com>
2019-04-08 10:45:25 -0700

    trest instantiation

--- DataStructures/BinaryTree/XUnitTestTree/UnitTest1.cs
+++ DataStructures/BinaryTree/XUnitTestTree/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using BinaryTree.Classes;
 
 namespace XUnitTestTree
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void BTinstantiation()
         {
+            BinaryTree<BTNode<int>> tree = new BinaryTree<BTNode<int>>();
+            Assert.IsType<BinaryTree<BTNode<int>>>(tree);
+        }
 
+        [Fact]
+        public void BSTinstantiation()
+        {
+            BinarySearchTree<BTNode<int>> tree = new BinarySearchTree<BTNode<int>>();
+            Assert.IsType<BinarySearchTree<BTNode<int>>>(tree);
         }
     }
 }
+
+
+//Can successfully instantiate a tree with a single root node
+//Can successfully add a left child and right child to a single root node
+//Can successfully return a collection from a preorder traversal
+//Can successfully return a collection from an inorder traversal
+//Can successfully return a collection from a postorder traversal
