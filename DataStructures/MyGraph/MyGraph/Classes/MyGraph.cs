230cda836ad9a10419f2f6e30f6a0224efa0034a
Jason Burns <jasonb315@protonmail.com>
2019-05-04 12:31:06 -0700

    graph class scaffold

---
+++ DataStructures/MyGraph/MyGraph/Classes/MyGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGraph.Classes
+{
+    class MyGraph<T>
+    {
+        public Dictionary<Vertex<T>, List<Edge<T>>> AdjacencyList { get; set; }
+        public int _size = 0;
+        public MyGraph()
+        {
+            AdjacencyList = new Dictionary<Vertex<T>, List<Edge<T>>>();
+        }
+
+        
+    }
+}
---
+++ DataStructures/MyGraph/MyGraph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
